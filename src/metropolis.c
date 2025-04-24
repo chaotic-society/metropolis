76ce9aba1f3c1145b3c2db362157b23f2988d5a2
Andrea Di Antonio <diantonioandrea@icloud.com>
2025-04-24 21:34:17 +0200

    Create metropolis.c

---
+++ src/metropolis.c
@@ -0,0 +1,11 @@
+/**
+ * @file metropolis.c
+ * @author Andrea Di Antonio (github.com/diantonioandrea), Mattia Isgrò (github.com/mattiaisgro)
+ * @brief 
+ * @date 2025-04-24
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include "metropolis.h"
