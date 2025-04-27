fc31216e946cdfc2794ca2ff1fee96f899949ecb
Andrea Di Antonio <diantonioandrea@icloud.com>
2025-04-27 11:24:48 +0200

    Update metropolis.c
    First metropolis prototype

--- src/metropolis.c
+++ src/metropolis.c
@@ -8,6 +8,11 @@
  * 
  */
 
+#if !defined(NDEBUG)
+#include <assert.h>
+#endif
+
+#include "random.h"
 #include "metropolis.h"
 
 /**
@@ -19,7 +24,24 @@
  * @param Natural Depth.
  */
 void metropolis(const real_t (*Function) (const vector_t*), vector_t* Vector_0, const vector_t* Vector_1, const unsigned int Natural) {
+    #if !defined(NDEBUG)
+    assert(Vector_0->size == Vector_1->size);
+    #endif
 
-    // [!]
-    
+    vector_t* Vector_2 = new_Vector(Vector_0->size);
+
+    for(unsigned int j = 0; j < Natural; ++j) {
+
+        // Sample.
+        random_gaussian(Vector_2, Vector_1);
+
+        // Proposal.
+        sum_Vector(Vector_2, Vector_0);
+
+        // Acceptance and step.
+        if(random_uniform() >= Function(Vector_2) / Function(Vector_0))
+            copy_Vector(Vector_0, Vector_2);
+    }
+
+    free_Vector(Vector_2);
 }
