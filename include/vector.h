0ae431f50d3e7adb4374204bef687186f8623ff3
Andrea Di Antonio <diantonioandrea@icloud.com>
2025-04-27 10:52:35 +0200

    Create vector.h

---
+++ include/vector.h
@@ -0,0 +1,45 @@
+/**
+ * @file vector.h
+ * @author Andrea Di Antonio (github.com/diantonioandrea), Mattia Isgrò (github.com/mattiaisgro)
+ * @brief Real vectors.
+ * @date 2025-04-27
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#if !defined(VECTOR_H)
+#define VECTOR_H
+
+// Real numbers.
+
+/**
+ * @brief Real numbers.
+ * 
+ */
+typedef double real_t;
+
+// Vectors.
+
+/**
+ * @brief Real vectors.
+ * 
+ */
+typedef struct Vector {
+    unsigned int size;
+    real_t* elements;
+} vector_t;
+
+// Vector methods.
+
+[[nodiscard]] vector_t* new_Vector(const unsigned int);
+void free_Vector(vector_t*);
+
+void sum_Vector(vector_t*, const vector_t*);
+void sub_Vector(vector_t*, const vector_t*);
+void mul_Vector(vector_t*, const real_t);
+void div_Vector(vector_t*, const real_t);
+
+real_t dot_Vector(const vector_t*, const vector_t*);
+
+#endif
