59c3619dfcdaab710fa61ab3cb1bf7bcc2206667
Andrea Di Antonio <diantonioandrea@icloud.com>
2025-04-27 11:00:33 +0200

    Update metropolis.c

--- src/metropolis.c
+++ src/metropolis.c
@@ -9,3 +9,17 @@
  */
 
 #include "metropolis.h"
+
+/**
+ * @brief Simple Metropolis.
+ * 
+ * @param Function Function.
+ * @param Vector_0 State.
+ * @param Vector_1 Deviations.
+ * @param Natural Depth.
+ */
+void metropolis(const real_t (*Function) (const vector_t*), vector_t* Vector_0, const vector_t* Vector_1, const unsigned int Natural) {
+
+    // [!]
+    
+}
